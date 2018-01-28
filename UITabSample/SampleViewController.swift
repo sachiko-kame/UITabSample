ce13126d8fa7e5754c785fd0fca721660e52ea4d
sachiko-kame <taiyousannsann2525sann@yahoo.co.jp>
2018-01-28 20:34:02 +0900

    使うためのViewControllerの作成
    パージビューコントローラーに表示するページ。ラベルテキスト変えて使い回すつもりです( ･ὢ･ )

---
+++ UITabSample/SampleViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SampleViewController.swift
+//  UITabSample
+//
+//  Created by 水野祥子 on 2018/01/28.
+//  Copyright © 2018年 sachiko. All rights reserved.
+//
+
+import UIKit
+
+class SampleViewController: UIViewController {
+    @IBOutlet weak var pageLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+}
