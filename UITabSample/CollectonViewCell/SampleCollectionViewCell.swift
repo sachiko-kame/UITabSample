e7e8b09d58fc28330699b3311001326f07a1d72c
sachiko-kame <taiyousannsann2525sann@yahoo.co.jp>
2018-01-28 20:39:58 +0900

    使うためのCollectionViewCellの作成とファイル移動
    パージビューコントローラーに表示するCell。ラベルテキスト変えて使い回すつもりです
    そのほかにフォルダ追加しました。(後で見やすくするためです。)

---
+++ UITabSample/CollectonViewCell/SampleCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  SampleCollectionViewCell.swift
+//  UITabSample
+//
+//  Created by 水野祥子 on 2018/01/28.
+//  Copyright © 2018年 sachiko. All rights reserved.
+//
+
+import UIKit
+
+class SampleCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var CategoryNameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
