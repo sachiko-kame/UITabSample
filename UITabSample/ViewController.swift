39b260dd33475dbac7345dd7cfc70d19c256193f
sachiko-kame <taiyousannsann2525sann@yahoo.co.jp>
2018-01-28 20:21:33 +0900

    Initial Commit

---
+++ UITabSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UITabSample
+//
+//  Created by 水野祥子 on 2018/01/28.
+//  Copyright © 2018年 sachiko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
