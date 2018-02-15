1228dbe5c132d1ca4b339ab4f0b6f1a9aaddfe42
sachiko-kame <taiyousannsann2525sann@yahoo.co.jp>
2018-02-16 00:35:41 +0900

    追加されていなかったファイルの追加とREAD.MEの修正をしました

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 ## Demo
 
 Depending on input, the output is displayed as the result like the image below.
+
+
 ![result](https://github.com/sachiko-kame/UITabSample/blob/736d03ff4cf2d7b81520dee0288af0183d9debcd/タブUIsampleNew.gif)
---
+++ UITabSample/defalt.swift
@@ -0,0 +1,12 @@
+//
+//  defalt.swift
+//  UITabSample
+//
+//  Created by 水野祥子 on 2018/02/11.
+//  Copyright © 2018年 sachiko. All rights reserved.
+//
+
+import UIKit
+
+let viewframewidth:CGFloat = UIScreen.main.bounds.size.width
+let statusBarHeight:CGFloat = UIApplication.shared.statusBarFrame.height
